e29e6d1a7a485b418af700d28b3633e6266a7926
paeducation <sgerio@gmx.de>
2026-09-15 15:58:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'notenbuch-v123';
+const CACHE_NAME = 'notenbuch-v125';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
